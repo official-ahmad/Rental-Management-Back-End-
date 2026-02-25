1e956c789d4863d5d870755a44ce9d527dae788b
Ahmad. <ahmadjuttboy516@gmail.com>
2026-02-25 12:46:05 +0500

    Updated

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(express.json());
 
 app.use(
   cors({
+    // origin: "http://localhost:5173",
     origin: "https://rental-management-front-end.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
